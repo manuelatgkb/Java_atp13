67117739a844c470474ce7b57c941ffabd37f97b
Manuela Briere <manuelatgkb@gmail.com>
2021-09-28 10:09:57 -0300

    inicial

---
+++ src/modulo1/MetodosMedia.java
@@ -0,0 +1,12 @@
+package modulo1;
+
+public class MetodosMedia{
+    public static void main(String[] args) {
+        
+    }
+
+    public static void cabecalho(){
+        System.out.println("_________________Cálculo de Média ____________________") 
+    }
+    
+}
