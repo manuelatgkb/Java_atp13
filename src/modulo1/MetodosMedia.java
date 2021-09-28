fe1674f6a483ab642af1ebde0286482891cdd7c4
Manuela Briere <manuelatgkb@gmail.com>
2021-09-28 10:31:56 -0300

    final

--- src/modulo1/MetodosMedia.java
+++ src/modulo1/MetodosMedia.java
@@ -3,12 +3,17 @@ package modulo1;
 public class MetodosMedia{
     public static void main(String[] args) {
         cabecalho();
-        somar();
+        int r1 = somar(10, 2);
+        int r2 = somar(5, 10);
+        int r3 = somar(7, 7);
+        int r4 = somar(10, 10);
+       double resultadofinal=  mediaAritmetica(r1, r2, r3, r4);
+        imprimir("O resultado da média é: " + resultadofinal);
         
     }
 
     public static void cabecalho(){
-        System.out.println("_________________Cálculo de Média ____________________") 
+        System.out.println("_________________Cálculo de Média ____________________");
     }
 
     public static int somar(int n1, int n2){
@@ -16,10 +21,14 @@ public class MetodosMedia{
        return resultado;
     }
 
-    public static double mediaAritmética(int n1, int n2, int n3,int n4) {
-        double resultado = Double.parseDouble(n1 + n2 + n3 + n4)/4;
+    public static double mediaAritmetica(int n1, int n2, int n3,int n4){
+        double resultado = (double)(n1 + n2 + n3 + n4)/4;
         return resultado;
     }
+
+    public static void imprimir(String mensagem) {
+        System.out.printf(mensagem);
         
     }
+        
 }
