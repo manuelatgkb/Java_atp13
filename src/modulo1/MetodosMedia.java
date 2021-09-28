0727d7c6e27814966f4ebbe5491c100b180e9336
Manuela Briere <manuelatgkb@gmail.com>
2021-09-28 10:17:56 -0300

    mediaaritmetica

--- src/modulo1/MetodosMedia.java
+++ src/modulo1/MetodosMedia.java
@@ -2,11 +2,24 @@ package modulo1;
 
 public class MetodosMedia{
     public static void main(String[] args) {
+        cabecalho();
+        somar();
         
     }
 
     public static void cabecalho(){
         System.out.println("_________________Cálculo de Média ____________________") 
     }
-    
+
+    public static int somar(int n1, int n2){
+       int resultado = n1 + n2;
+       return resultado;
+    }
+
+    public static double mediaAritmética(int n1, int n2, int n3,int n4) {
+        double resultado = Double.parseDouble(n1 + n2 + n3 + n4)/4;
+        return resultado;
+    }
+        
+    }
 }
